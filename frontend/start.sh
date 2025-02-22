b69c54990889b68679acd33056a4f151119971a3
Venus-Hu <68754382+Venus-Hu@users.noreply.github.com>
2025-02-22 12:20:57 -0500

    bashing

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,2 +1,2 @@
-echo "Starting the Node.js server..."
-nohup npm run start > server.log 2>&1 &
+echo "Starting FE..."
+npm run start
--- healthtrack-back/start.sh
+++ healthtrack-back/start.sh
@@ -1,2 +1,2 @@
 echo "Starting the Node.js server..."
-nohup node server.js > server.log 2>&1 &
+node server.js
